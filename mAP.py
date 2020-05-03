d1aff2519adac5d70be6431ae3fe43edc5a03f55
NCTU-VRDL <55061120+NCTU-VRDL@users.noreply.github.com>
2020-05-04 01:43:44 +0800

    Update mAP.py

--- mAP.py
+++ mAP.py
@@ -288,7 +288,7 @@ if show_animation:
    Create a list of all the class names present in the ground-truth (gt_classes).
 """
 # get a list with the ground-truth files
-ground_truth_files_list = glob.glob('ground-truth/*.txt')
+ground_truth_files_list = glob.glob('mAP/ground-truth/*.txt')
 if len(ground_truth_files_list) == 0:
   error("Error: No ground-truth files found!")
 ground_truth_files_list.sort()
@@ -300,7 +300,7 @@ for txt_file in ground_truth_files_list:
   file_id = txt_file.split(".txt",1)[0]
   file_id = os.path.basename(os.path.normpath(file_id))
   # check if there is a correspondent predicted objects file
-  if not os.path.exists('predicted/' + file_id + ".txt"):
+  if not os.path.exists('mAP/predicted/' + file_id + ".txt"):
     error_msg = "Error. File not found: predicted/" +  file_id + ".txt\n"
     error_msg += "(You can avoid this error message by running extra/intersect-gt-and-pred.py)"
     error(error_msg)
@@ -377,7 +377,7 @@ if specific_iou_flagged:
    Load each of the predicted files into a temporary ".json" file.
 """
 # get a list with the predicted files
-predicted_files_list = glob.glob('predicted/*.txt')
+predicted_files_list = glob.glob('mAP/predicted/*.txt')
 predicted_files_list.sort()
 
 for class_index, class_name in enumerate(gt_classes):
@@ -388,7 +388,7 @@ for class_index, class_name in enumerate(gt_classes):
     file_id = txt_file.split(".txt",1)[0]
     file_id = os.path.basename(os.path.normpath(file_id))
     if class_index == 0:
-      if not os.path.exists('ground-truth/' + file_id + ".txt"):
+      if not os.path.exists('mAP/ground-truth/' + file_id + ".txt"):
         error_msg = "Error. File not found: ground-truth/" +  file_id + ".txt\n"
         error_msg += "(You can avoid this error message by running extra/intersect-gt-and-pred.py)"
         error(error_msg)
